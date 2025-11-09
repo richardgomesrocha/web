962ad829c8c538695336d9fde7527fa83073907d
Richard  gomes <Rg931512@gmail.com>
2025-11-09 10:28:13 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,68 @@
+const exphbs = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.use(express,json());
+app.use(express.urlencoded({ extended: true}));
+
+app,engine('handlebars', exphbs.engine({ defaultLayout: false}));
+app,set('view engine', 'handlebars');
+
+let pessoas = [
+    { id: 1, nome: "Pessoa 1" },
+    { id: 2, nome: "Pessoa 2" },
+    { id: 3, nome: "Pessoa 3" },
+];
+
+
+app.get('/',(req, res) => res.render('home'));
+
+app,get('/pessoas', (req,res) => {
+    res.render('listarPessoas',{ pessoas});
+});
+
+app.get('/pessoas/nova', (req,res) => res.render('cadastrarPessoa'));
+
+app.post('/pessoas', (req,res) => {
+    const {nome}= req.body;
+    const novaPessoa = {id: pessoas.length + 1, nome};
+    pessoas.push(novaPessoa);
+    res.render('listarPessoas', {pessoas});
+});
+
+app.get('/pessoas/ver/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const pessoa = pessoas.find(p =>p.id === id);
+    if(!pessoa) return res.status(404).send('Pessoa não encontrada');
+
+    res.render('detalharPessoa',{ pessoa });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console,log('servidor em execução: http://localhost:${port}');
+});
